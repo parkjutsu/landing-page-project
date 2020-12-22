4414232c4c6e721e6bc7cc7f1329396bd967e5c9
Abraham Park <65786838+parkjutsu@users.noreply.github.com>
2020-12-21 16:21:11 -0800

    Add navbar and scroll behaviour

--- js/app.js
+++ js/app.js
@@ -17,7 +17,8 @@
  * Define Global Variables
  * 
 */
-
+const navList = document.querySelector('#navbar__list');
+const sections = document.querySelectorAll('section');
 
 /**
  * End Global Variables
@@ -34,13 +35,48 @@
 */
 
 // build the nav
+const buildNav = () => {
+    const docFrag = document.createDocumentFragment();
 
+    for (const section of sections) {
+        const navItem = document.createElement('li');
+        const anchor = document.createElement('a');
+
+        anchor.href = `#${section.id}`;
+        anchor.innerText = section.dataset.nav;
+        // Using HTML data-* attribute to store section id
+        anchor.dataset.id = section.id;
+
+        navItem.appendChild(anchor);
+        docFrag.appendChild(navItem);
+    }
+    navList.appendChild(docFrag);
+};
 
 // Add class 'active' to section when near top of viewport
+const addActiveClass = (activeSection) => {
+    // const listOfClasses = section.classList;
 
+    // sections.forEach(section => {
+    //     if (!listOfClasses.contains('active')) {
+    //         listOfClasses.add('active');
+    //     }
+    // });
+};
 
 // Scroll to anchor ID using scrollTO event
-
+const scrollToAnchor = (event) => {
+    if (event.target.nodeName === 'A') {
+        event.preventDefault();
+        
+        const anchorElement = document.getElementById(event.target.dataset.id);
+        anchorElement.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest"
+        });
+    }
+};
 
 /**
  * End Main Functions
@@ -49,8 +85,10 @@
 */
 
 // Build menu 
+buildNav();
 
 // Scroll to section on link click
+navList.addEventListener('click', scrollToAnchor);
 
 // Set sections as active
 
